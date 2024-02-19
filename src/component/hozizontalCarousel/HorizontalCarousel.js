571d3065b3bd430c96de195df8440f52e3b949bd
tanaykulkarni27 <1002kulkarni@gmail.com>
2024-02-19 17:00:19 +0530

    init

--- src/component/hozizontalCarousel/HorizontalCarousel.js
+++ src/component/hozizontalCarousel/HorizontalCarousel.js
@@ -1,16 +1,47 @@
 // HorizontalCarousel.js
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import './horizontalCarousel.css'
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./horizontalCarousel.css";
+
+const NextSlide = ({ onClick }) => (
+  <button
+    onClick={onClick}
+    className="slick-arrow-next rounded-full rounded-full bg-white p-1"
+  >
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+    >
+      <path d="M4 11H16.17l-5.59-5.59L12 4l8 8-8 8-1.41-1.41L16.17 13H4z" />
+    </svg>
+  </button>
+);
+const PrevSlide = ({ onClick }) => (
+  <button onClick={onClick} className="slick-arrow-prev rounded-full bg-white">
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+    >
+      <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z" />
+    </svg>
+  </button>
+);
 
 const HorizontalCarousel = ({ items }) => {
   const settings = {
-    arrows:false,
+    prevArrow: <PrevSlide />,
+    nextArrow: <NextSlide />,
     infinite: true,
     speed: 300,
-    slidesToShow: 5, // Set the number of items to show at a time
+    slidesToShow: 6, // Set the number of items to show at a time
     slidesToScroll: 1,
     responsive: [
       {
@@ -38,16 +69,22 @@ const HorizontalCarousel = ({ items }) => {
   };
 
   return (
-    <div className='ml-5'>
-        <div>
-            <Slider {...settings} >
-            {items.map((item, index) => (
-                <div key={index} className="carousel-item rounded-lg">
-                    <video src={item} className='rounded-xl' style={{width:'15vw',height:'auto'}} autoPlay loop />
-                </div>
-            ))}
-            </Slider>
-        </div>
+    <div className="ml-5">
+      <div>
+        <Slider {...settings}>
+          {items.map((item, index) => (
+            <div key={index} className="carousel-item rounded-lg">
+              <video
+                src={item}
+                className="rounded-xl"
+                style={{ width: "15vw", height: "auto" }}
+                autoPlay
+                loop
+              />
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
